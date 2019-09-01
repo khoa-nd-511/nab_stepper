0763096a28583d3c28f53b3e5bced5334eb60ef7
Khoa Nguyen <khoa.nguyen@pickupp.io>
2019-09-01 20:35:28 +0700

    final touch

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import { Stepper } from './components/Stepper'
 import { theme } from './theme'
 import { styles } from './styles'
 
+const STEPS = [
+	{ number: 1, label: 'Supplier', visited: true, activated: true },
+	{ number: 2, label: 'Basic Infomation', visited: false, activated: false },
+	{ number: 3, label: 'Language', visited: false, activated: false },
+	{ number: 4, label: 'Lorem ispsum', visited: false, activated: false },
+	{ number: 5, label: 'Some dumb text', visited: false, activated: false },
+	{ number: 6, label: 'Vertical Stepper', visited: false, activated: false },
+]
+
 const App = ({ classes }) => {
 	return (
 		<div className={classes.wrapper}>
-			<Stepper />
+			<Stepper source={STEPS} />
 		</div>
 	)
 }
--- src/components/Stepper/index.js
+++ src/components/Stepper/index.js
@@ -3,21 +3,13 @@ import withStyles from 'react-jss'
 
 import { Step } from '../Step'
 import { styles } from './styles'
-const STEPS = [
-	{ number: 1, label: 'Supplier', visited: true, activated: true },
-	{ number: 2, label: 'Basic Infomation', visited: false, activated: false },
-	{ number: 3, label: 'Language', visited: false, activated: false },
-	{ number: 4, label: 'Lorem ispsum', visited: false, activated: false },
-	{ number: 5, label: 'Some dumb text', visited: false, activated: false },
-	{ number: 6, label: 'Vertical Stepper', visited: false, activated: false },
-]
 
-const StepperComp = ({ classes }) => {
+const StepperComp = ({ classes, source }) => {
 	const [width, setWidth] = useState(0)
 	const ref = useRef()
-	const [steps, setActiveStep] = useState(STEPS)
+	const [steps, setActiveStep] = useState(source)
 
-	const updateWidth = () => setWidth(ref.current.clientWidth / STEPS.length)
+	const updateWidth = () => setWidth(ref.current.clientWidth / steps.length)
 
 	useEffect(() => {
 		updateWidth()
